4ae411f86038fef563e08094979233cbb7b1cb90
Péli Ferenc <peli.ferenc92@gmail.com>
2020-10-08 18:44:14 +0200

    First task

--- Tarsalgo-2018/Entry.cs
+++ Tarsalgo-2018/Entry.cs
@@ -1,6 +1,22 @@
-﻿namespace Tarsalgo_2018
+﻿using System;
+
+namespace Tarsalgo_2018
 {
     public class Entry
     {
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+        public int Id { get; set; }
+        public bool IsIncoming { get; set; }
+
+        public Entry(string line)
+        {
+            var values = line.Split(' ');
+
+            Hours = Convert.ToInt32(values[0]);
+            Minutes = Convert.ToInt32(values[1]);
+            Id = Convert.ToInt32(values[2]);
+            IsIncoming = values[3] == "be";
+        }
     }
 }
--- Tarsalgo-2018/Program.cs
+++ Tarsalgo-2018/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Tarsalgo_2018
 {
     class Program
     {
+        static List<Entry> Entries = new List<Entry>();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ReadFilesFromFile();
+
+            Task1();
+            Task2();
+        }
+
+        private static void Task1()
+        {
+            Console.WriteLine($"Az első belépő: {Entries.First(x => x.IsIncoming).Id}");
+            Console.WriteLine($"Az utolsó kilépő: {Entries.Last(x => !x.IsIncoming).Id}");
+        }
+
+        private static void Task2()
+        {
+
+        }
+
+        private static void ReadFilesFromFile()
+        {
+            using StreamReader o = new StreamReader("ajto.txt");
+            while (!o.EndOfStream)
+            {
+                Entries.Add(new Entry(o.ReadLine()));
+            }
         }
     }
 }
